a9b08f7b0dbbcba100cde4f4f18ade57140b60b1
LuanFabricio <luanlimaleite@hotmail.com>
2024-02-24 20:09:22 -0300

    feat: Adding Point, AABB and QuadTree types

--- src/types.h
+++ src/types.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "raylib.h"
+#include "./defines.h"
 
 typedef struct {
 	size_t steps;
@@ -14,5 +15,24 @@ typedef struct {
 typedef struct {
 	Vector3 position, size;
 } Object;
+
+typedef struct {
+	float x, y;
+} Point;
+
+typedef struct {
+	Point center;
+	float half_dimension;
+} AABB;
+
+typedef struct QuadTree {
+	AABB node;
+	size_t points_size;
+	Point *points[NODE_CAPACITY];
+	struct QuadTree* north_west;
+	struct QuadTree* north_east;
+	struct QuadTree* south_west;
+	struct QuadTree* south_east;
+} QuadTree;
 
 #endif // TYPES_H_
